ca4466b219a8c7c097783b2f7d9b2cac7f430ced
Cedric Boucher <cedricjboucher@pm.me>
2024-09-26 17:51:22 -0600

    add colour property to Shape class

--- app/src/main/java/com/example/cooked0/Shape.java
+++ app/src/main/java/com/example/cooked0/Shape.java
@@ -3,6 +3,7 @@ package com.example.cooked0;
 public abstract class Shape {
     private Integer x;
     private Integer y;
+    String color = "blue";
 
     public Shape(Integer x, Integer y) {
         this.x = x;
