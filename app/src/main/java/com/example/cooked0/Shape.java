f1bf0a8e5fb072de6ed101757a78ad89f9df63be
Cedric Boucher <cedricjboucher@pm.me>
2024-09-26 17:19:51 -0600

    create template Shape class

---
+++ app/src/main/java/com/example/cooked0/Shape.java
@@ -0,0 +1,11 @@
+package com.example.cooked0;
+
+public abstract class Shape {
+    private Integer x;
+    private Integer y;
+
+    public Shape(Integer x, Integer y) {
+        this.x = x;
+        this.y = y;
+    }
+}
