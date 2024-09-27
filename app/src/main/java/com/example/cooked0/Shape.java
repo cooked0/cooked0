a61cf562520e26eab2721cc748fc0f9e575f19c7
Brandon Pard <bpard@ualberta.ca>
2024-09-26 18:23:06 -0600

    add colour

--- app/src/main/java/com/example/cooked0/Shape.java
+++ app/src/main/java/com/example/cooked0/Shape.java
@@ -3,9 +3,11 @@ package com.example.cooked0;
 public abstract class Shape {
     private Integer x;
     private Integer y;
+    private String Colour;
 
     public Shape(Integer x, Integer y) {
         this.x = x;
         this.y = y;
+        String Colour="blue";
     }
 }
