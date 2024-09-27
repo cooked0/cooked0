49550a87aa183d4097a80ebb26e03b68527bd68e
DanTang212 <daniyalabbas.share@gmail.com>
2024-09-26 18:36:06 -0600

    colour added

--- app/src/main/java/com/example/cooked0/Rhombus.java
+++ app/src/main/java/com/example/cooked0/Rhombus.java
@@ -2,4 +2,8 @@ public class Rhombus extends Shape{
     
     public Rhombus (Integer x, Integer y){
         super(x,y);
+
+
+
+
     }
--- app/src/main/java/com/example/cooked0/Shape.java
+++ app/src/main/java/com/example/cooked0/Shape.java
@@ -4,6 +4,9 @@ public abstract class Shape {
     private Integer x;
     private Integer y;
 
+    String color = "pink";
+
+
     public Shape(Integer x, Integer y) {
         this.x = x;
         this.y = y;
